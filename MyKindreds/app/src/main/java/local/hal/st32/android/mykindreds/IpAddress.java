8a724f36ad604c1581f0e8880d06a5404b1c51ee
ueshima <re-to-mikan@hotmail.co.jp>
2016-12-15 12:07:56 +0900

    add IpAddress.java アドレスのコモンクラス作成

---
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/IpAddress.java
@@ -0,0 +1,11 @@
+package local.hal.st32.android.mykindreds;
+
+/**
+ * Created by Tester on 2016/12/15.
+ *
+ *
+ */
+
+public interface IpAddress {
+    public static final String ipAddress = "";
+}
