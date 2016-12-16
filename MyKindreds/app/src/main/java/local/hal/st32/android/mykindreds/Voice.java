230479954dc92e3f82bec50c10db85dbf06793ae
ueshima <re-to-mikan@hotmail.co.jp>
2016-12-16 16:31:38 +0900

    add Voice.java ボイス用顧問クラスファイル作成

---
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/Voice.java
@@ -0,0 +1,12 @@
+package local.hal.st32.android.mykindreds;
+
+/**
+ * Created by Tester on 2016/12/15.
+ * 話す音声用のインターフェースクラス
+ */
+
+public interface Voice {
+    public static final String voiceHello = "おはようございますマスター";
+    public static final String voiceBay = "お疲れ様でした";
+
+}
