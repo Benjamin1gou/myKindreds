03796c1845ad2d83b1fc895fba3af11c52f41fc2
ueshima <re-to-mikan@hotmail.co.jp>
2016-12-16 17:37:20 +0900

    add AkaneFunction.java クラスファイル追加

---
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/AkaneFunction.java
@@ -0,0 +1,26 @@
+package local.hal.st32.android.mykindreds;
+
+import android.content.Context;
+
+/**
+ * Created by Tester on 2016/12/16.
+ * 音声入力されたデータによって機能を分岐させるクラス
+ *
+ */
+
+public class AkaneFunction {
+
+    public String method;
+
+    public Context context;
+
+    public AkaneFunction(String voice, Context context){
+        this.method = voice;
+        this.context = context;
+    }
+
+    public void
+
+
+
+}
