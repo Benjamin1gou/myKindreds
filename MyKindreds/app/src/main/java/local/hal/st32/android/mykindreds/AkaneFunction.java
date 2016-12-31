999dd88d8dece3e859d280f2e4f8afe442907c42
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-01 08:51:35 +0900

    add AkaneFunction.java 音声情報により機能分岐を行うメソッドの追加

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/AkaneFunction.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/AkaneFunction.java
@@ -1,25 +1,35 @@
-package local.hal.st32.android.mykindreds;
-
-import android.content.Context;
-
 /**
  * Created by Tester on 2016/12/16.
  * 音声入力されたデータによって機能を分岐させるクラス
  *
  */
+package local.hal.st32.android.mykindreds;
+
+import android.content.Context;
+
 
 public class AkaneFunction {
 
-    public String method;
-
     public Context context;
 
-    public AkaneFunction(String voice, Context context){
-        this.method = voice;
+    public AkaneFunction(Context context){
         this.context = context;
     }
 
-    public void
+    /**
+     * 音声で機能を分岐させるメソッド
+     * @param voice
+     */
+    public void methodSwitch(String voice){
+        switch (voice){
+            case "天気":
+
+                break;
+
+            default:
+                break;
+        }
+    }
 
 
 
