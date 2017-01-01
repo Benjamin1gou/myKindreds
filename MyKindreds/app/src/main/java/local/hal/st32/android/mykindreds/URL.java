ff2cbd9bc55a4b6a3c868553dd23aae152b0901d
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-01 09:19:55 +0900

    add URL.java URLインターフェースの作成

---
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/URL.java
@@ -0,0 +1,9 @@
+package local.hal.st32.android.mykindreds;
+
+/**
+ * Created by Tester on 2017/01/01.
+ */
+
+public interface URL {
+    public static final String WEATHER_URL ="http://weather.livedoor.com/forecast/webservice/json/v1";
+}
