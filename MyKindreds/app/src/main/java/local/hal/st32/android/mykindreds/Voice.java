9d794005c2439d2e8516f0edfe24d2602654aa83
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-01 09:20:12 +0900

    add voice.java ボイス追加

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/Voice.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/Voice.java
@@ -8,5 +8,7 @@ package local.hal.st32.android.mykindreds;
 public interface Voice {
     public static final String voiceHello = "おはようございますマスター";
     public static final String voiceBay = "お疲れ様でした";
+    public static final String voiceNone = "該当する機能が存在しませんマスター";
+    public static final String voiceWeather = "今日の天気をお伝えします";
 
 }
