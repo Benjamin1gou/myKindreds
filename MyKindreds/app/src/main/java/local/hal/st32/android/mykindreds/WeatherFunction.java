7cb2ac158dbfcb1e95acceb2ac17e572bd6c9874
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-10 10:57:05 +0900

    add WeatherFunction.java 天気パターン追加

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/WeatherFunction.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/WeatherFunction.java
@@ -5,17 +5,24 @@
 package local.hal.st32.android.mykindreds;
 
 
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.widget.ImageView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static local.hal.st32.android.mykindreds.MainActivity.tts;
+
 
 public class WeatherFunction {
 
+    private ImageView weatherIcon;
+
     public void startWeather(){
         Weather weather = new Weather();
+        weatherIcon = MainActivity.icon;
         //ここ修復まち　引数一つのためくっつけるように変更
         weather.execute(URL.WEATHER_URL, "270000");
     }
@@ -45,16 +52,66 @@ public class WeatherFunction {
             String msg = "今日の天気は" + telop + "\n" + text;
 
             weatherOutPut(telop,msg);
-
+            tts.speak(msg, TextToSpeech.QUEUE_ADD, null, msg);
         }
 
     }
 
+    /**
+     * 取得した天気情報を取得するメソッド
+     * @param telop
+     * @param msg
+     */
     public void weatherOutPut(String telop, String msg){
-
+        MainActivity.character.setImageResource(R.drawable.akane_speak);
+        msg = weatherSwitch(telop);
     }
 
-    public  void weatherSwitch(String telop){
-
+    /**
+     * 天気によって出力する天気のアイコンを分岐させるメソッド
+     * @param telop
+     */
+    public String weatherSwitch(String telop){
+        String msg = "";
+        switch (telop){
+            case "晴れ":
+                weatherIcon.setImageResource(R.drawable.tenki_sun);
+                break;
+            case "曇り":
+                weatherIcon.setImageResource(R.drawable.tenki_kumo);
+                break;
+            case "雨":
+                weatherIcon.setImageResource(R.drawable.tenki_ame);
+                break;
+            case "雪":
+                weatherIcon.setImageResource(R.drawable.tenki_yuki);
+                break;
+            case "晴のち曇":
+                weatherIcon.setImageResource(R.drawable.tenki_kumo);
+                break;
+            case "晴のち雨":
+                weatherIcon.setImageResource(R.drawable.tenki_ame);
+                break;
+            case "曇時々晴":
+                weatherIcon.setImageResource(R.drawable.tenki_sun);
+                break;
+            case "曇時々雨":
+                weatherIcon.setImageResource(R.drawable.tenki_ame);
+                break;
+            case "曇のち雨":
+                weatherIcon.setImageResource(R.drawable.tenki_ame);
+                break;
+            case "雨時々曇":
+                weatherIcon.setImageResource(R.drawable.tenki_kumo);
+                break;
+            case "暴風雨":
+                weatherIcon.setImageResource(R.drawable.tenki_bohu);
+                break;
+            default:
+                weatherIcon.setImageResource(R.drawable.question);
+                msg = "知らないパターンです。新しく登録お願いしますマスター" + telop;
+                break;
+        }
+        return msg;
     }
 }
