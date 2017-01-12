2cb52073a3c989a76ce7913170adee21a1bfbe95
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-12 11:06:18 +0900

    add DatabaseHelper.java	DB用javaクラス作成

---
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/DatabaseHelper.java
@@ -0,0 +1,11 @@
+package local.hal.st32.android.mykindreds;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Tester on 2017/01/12.
+ * content:
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+}
