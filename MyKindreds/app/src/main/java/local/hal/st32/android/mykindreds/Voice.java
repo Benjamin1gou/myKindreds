dbdde19105042720f3d130080bd5038e39c6b411
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-17 12:00:35 +0900

    add バルス音声追加

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/Voice.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/Voice.java
@@ -10,5 +10,6 @@ public interface Voice {
     public static final String voiceBay = "お疲れ様でした";
     public static final String voiceNone = "該当する機能が存在しませんマスター";
     public static final String voiceWeather = "今日の天気をお伝えします";
+    public static final String voiceBals = "うわぁ目がー目がー";
 
 }
