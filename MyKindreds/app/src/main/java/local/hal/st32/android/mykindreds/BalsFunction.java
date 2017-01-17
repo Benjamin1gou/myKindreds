6150b16d84f40920ebcff7379212b1a6d5d599d6
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-17 12:00:20 +0900

    add バルス実装

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/BalsFunction.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/BalsFunction.java
@@ -1,8 +1,16 @@
 package local.hal.st32.android.mykindreds;
 
+import android.speech.tts.TextToSpeech;
+
 /**
  * Created by Tester on 2017/01/17.
  */
 
 public class BalsFunction {
+
+    public void bals(){
+        MainActivity.character.setImageResource(R.drawable.akane_bals);
+        MainActivity.speakText.setText(Voice.voiceBals);
+        MainActivity.tts.speak(Voice.voiceBals, TextToSpeech.QUEUE_ADD, null, Voice.voiceBals);
+    }
 }
