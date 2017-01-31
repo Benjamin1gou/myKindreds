c214fec8a66e51520f189b3725a21ed82867be92
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-01 05:16:03 +0900

    add 履歴を表示実装

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/WebHistoryFunction.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/WebHistoryFunction.java
@@ -1,6 +1,16 @@
 package local.hal.st32.android.mykindreds;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * web履歴表示用クラス
@@ -10,16 +20,37 @@ import java.util.ArrayList;
 public class WebHistoryFunction {
 
     public static final String METHOD = "WebHistoryFunction";
+    ListView _list;
 
     private MainActivity main;
-    private ArrayList<String> result;
+    private ArrayList<Map<String,String>> result;
 
     public WebHistoryFunction(MainActivity main){
         this.main = main;
     }
 
     public void historyStart(){
+        _list = (ListView)main.findViewById(R.id.todoList);
         result = DataAccess.historyGet(main);
+        outputList(result);
 
+    }
+
+    public void outputList(List<Map<String,String>> listDate){
+        String[] from = {"word"};
+        int[] to = {android.R.id.text1};
+        SimpleAdapter adapter = new SimpleAdapter(main, listDate, android.R.layout.simple_list_item_1, from, to);
+        _list.setAdapter(adapter);
+        _list.setOnItemClickListener(new WebHistoryFunction.ListViewOnClickListener());
+    }
+
+    private class ListViewOnClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Map<String, String> intentData = result.get(position);
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, intentData.get("word"));
+            main.startActivity(intent);
+        }
     }
 }
