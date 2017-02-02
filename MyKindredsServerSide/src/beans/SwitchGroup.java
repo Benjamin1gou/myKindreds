907fc898064e52ba20c0b724c581a720e00b3b0a
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-03 01:04:02 +0900

    add ServerSide グループ機能用クラス作成

---
+++ MyKindredsServerSide/src/beans/SwitchGroup.java
@@ -0,0 +1,11 @@
+/***************************
+ * ClassName:
+ * Creater: Tester
+ * Createdate: 2017/02/03
+ * content: 
+ * *************************/
+package beans;
+
+public class SwitchGroup {
+
+}
