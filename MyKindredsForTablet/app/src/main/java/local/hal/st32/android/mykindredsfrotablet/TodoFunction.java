8d5f5560253c143ebe5eb4f0b60b5e3ea5b923f1
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-13 07:12:03 +0900

    add Todoの削除をタップでできるように変更

--- MyKindredsForTablet/app/src/main/java/local/hal/st32/android/mykindredsfrotablet/TodoFunction.java
+++ MyKindredsForTablet/app/src/main/java/local/hal/st32/android/mykindredsfrotablet/TodoFunction.java
@@ -111,6 +111,7 @@ public class TodoFunction {
                     outputMessage(message);
                     break;
                 case BROWSE:
+                    re.setRequestId("id");
                     re.setRequestId("title");
                     list = re.json(result, "data");
                     outputList(list);
@@ -138,9 +139,13 @@ public class TodoFunction {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             Map<String, String> intentData = list.get(position);
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            intent.putExtra(SearchManager.QUERY, intentData.get("title"));
-            main.startActivity(intent);
+            Log.e("map", ""+intentData);
+            int line = Integer.parseInt(intentData.get("id"));
+
+            serverAccess serve = new serverAccess();
+            Log.d("ipaddress",URL.Todo_URL+"?method="+METHOD+"&userId="+User.userData+"&type="+DELETE+"&mission="+"");
+            serve.execute(URL.Todo_URL+"?method="+METHOD+"&userId="+User.userData+"&type="+DELETE+"&mission="+""+"&line="+line);
+            METHOD_NAME = DELETE;
         }
     }
 
