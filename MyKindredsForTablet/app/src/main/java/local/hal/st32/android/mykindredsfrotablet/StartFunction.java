9ce994e0e1d47bf9a906a7f27d065149fee2bf48
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-13 07:12:33 +0900

    add ソース追加

--- MyKindredsForTablet/app/src/main/java/local/hal/st32/android/mykindredsfrotablet/StartFunction.java
+++ MyKindredsForTablet/app/src/main/java/local/hal/st32/android/mykindredsfrotablet/StartFunction.java
@@ -1,5 +1,16 @@
 package local.hal.st32.android.mykindredsfrotablet;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * アプリが起動または再開された際に動くクラス
  * Created by Tester on 2017/01/31.
@@ -7,7 +18,20 @@ package local.hal.st32.android.mykindredsfrotablet;
 
 public class StartFunction {
 
-    public static void start(MainActivity main){
+    ListView todo;
+    ListView memo;
+    ListView history;
+    ListView group;
+    List<Map<String, String>> oneColumnList;
+    MainActivity main;
+
+
+    public StartFunction(MainActivity main){
+        this.main = main;
+    }
+
+
+    public  void start(){
         /**
          * todo メモ表示
          */
@@ -16,18 +40,55 @@ public class StartFunction {
         /**
          * todo 検索履歴表示
          */
+        WebHistoryFunction history = new WebHistoryFunction(main);
+        history.historyStart();
 
 
-        /**
-         * todo 天気表示
-         */
         WeatherFunction weather = new WeatherFunction();
-        weather.startWeather();
+        weather.startWeather("START");
 
 
         /**
          * todo todo,グループ表示
          */
+        todo = (ListView)main.findViewById(R.id.todoList);
+        memo = (ListView)main.findViewById(R.id.memoList);
+        group =(ListView)main.findViewById(R.id.GroupList);
+        ServerAccess access = new ServerAccess();
+        //todo ここ引数入れる
+        access.execute(URL.Todo_URL+"?method=Start&userId="+User.userData);
+//        access.execute(URL.Todo_URL+"?method=Todo&userId="+User.userData+"&type=BROWSE&mission="+" ");
+
+    }
+
+    private class ServerAccess extends Access {
+        @Override
+        public void onPostExecute(String result){
+            Replace re = new Replace();
+            re.setRequestId("title");
+            oneColumnList = re.json(result, "todo");
+            outputTodoList(oneColumnList);
+            oneColumnList = re.json(result, "memo");
+            outputMemoList(oneColumnList);
+
+        }
+    }
+
+    public void outputTodoList(List<Map<String,String>> listDate) {
+        String[] from = {"title"};
+        int[] to = {android.R.id.text1};
+        SimpleAdapter adapter = new SimpleAdapter(main, listDate, android.R.layout.simple_list_item_1, from, to);
+        todo.setAdapter(adapter);
+//        todo.setOnItemClickListener(new TodoFunction.ListViewOnClickListener());
+    }
+
+
+    public void outputMemoList(List<Map<String,String>> listDate) {
+        String[] from = {"title"};
+        int[] to = {android.R.id.text1};
+        SimpleAdapter adapter = new SimpleAdapter(main, listDate, android.R.layout.simple_list_item_1, from, to);
+        memo.setAdapter(adapter);
+//        todo.setOnItemClickListener(new TodoFunction.ListViewOnClickListener());
     }
 
 
