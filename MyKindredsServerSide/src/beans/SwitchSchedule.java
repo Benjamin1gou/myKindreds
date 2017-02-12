5f6781f27061e1acdbc807378dd5c8c1454350cf
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-13 07:11:35 +0900

    add 予定ファンクション用クラス作成

---
+++ MyKindredsServerSide/src/beans/SwitchSchedule.java
@@ -0,0 +1,11 @@
+/***************************
+ * ClassName: SwitchSchedule
+ * Creater: Tester
+ * Createdate: 2017/02/13
+ * content: ユーザの予定を操作するクラス
+ * *************************/
+package beans;
+
+public class SwitchSchedule {
+
+}
