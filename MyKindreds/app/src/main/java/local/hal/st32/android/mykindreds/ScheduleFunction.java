cd2c981b7983815067331df7424c529456cf70fe
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-16 15:43:49 +0900

    change 音声情報「予定」のみの場合、当日の予定を教えてくれるように変更

--- MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/ScheduleFunction.java
+++ MyKindreds/app/src/main/java/local/hal/st32/android/mykindreds/ScheduleFunction.java
@@ -1,7 +1,9 @@
 package local.hal.st32.android.mykindreds;
 
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -55,7 +57,7 @@ public class ScheduleFunction {
 
     public void ScheduleInsert(String voice){
         String date = metamorDate(voice);
-        String schedule = Replace.pullMission(voice, DATEINT+3, _add);
+        String schedule = Replace.pullMission(voice, DATEINT, _add);
         METHOD_NAME = ADD;
         ServerAccess serv = new ServerAccess();
         serv.execute(URL.Todo_URL+"?method="+METHOD+"&userId="+User.userData+"&type="+ADD+"&date="+date+"&schedule="+schedule);
@@ -63,6 +65,9 @@ public class ScheduleFunction {
 
     public void ScheduleBrowse(String voice){
         String date = metamorDate(voice);
+        METHOD_NAME = BROWSE;
+        ServerAccess serv = new ServerAccess();
+        serv.execute(URL.Todo_URL+"?method="+METHOD+"&userId="+User.userData+"&type="+BROWSE+"&date="+date+"&schedule="+" ");
     }
 
 
@@ -80,21 +85,26 @@ public class ScheduleFunction {
             DATENAME = TODAY;
             dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
             Log.d("今日",dateTime);
-            DATEINT = 2;
+            DATEINT = 5;
         }else if(0 <= voice.indexOf(TOMORROW)){
             DATENAME = TOMORROW;
-            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+2) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
+            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + (calendar.get(Calendar.DAY_OF_MONTH)+1);
             Log.d("明日",dateTime);
-            DATEINT = 2;
+            DATEINT = 5;
         }else if(0 <= voice.indexOf(YESTERDAY)){
             DATENAME = YESTERDAY;
-            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)-1) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
+            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + (calendar.get(Calendar.DAY_OF_MONTH)+2);
             Log.d("明後日",dateTime);
-            DATEINT = 3;
+            DATEINT = 6;
         }else if(0 <= voice.indexOf(AFTERTOMORROW)){
             DATENAME = AFTERTOMORROW;
-            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+3) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
+            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + (calendar.get(Calendar.DAY_OF_MONTH)-1);
             Log.d("昨日",dateTime);
+            DATEINT = 5;
+        }else{
+            DATENAME = TODAY;
+            dateTime = calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
+            Log.d("今日",dateTime);
             DATEINT = 2;
         }
         return dateTime;
@@ -107,17 +117,17 @@ public class ScheduleFunction {
             String message;
             switch(METHOD_NAME){
                 case ADD:
-                    re.setRequestId("groupName");
-                    message = re.json(result,"data").get(0).get("groupName");
+                    re.setRequestId("title");
+                    message = re.json(result,"data").get(0).get("title");
                     outputMessage(message);
                     break;
-                case GSEARCH:
-                    re.setRequestId("groupName");
-                    list = re.json(result, "data");
-                    outputList(list);
-                    break;
-                case GLIST:
-                    re.setRequestId("groupName");
+//                case GSEARCH:
+//                    re.setRequestId("groupName");
+//                    list = re.json(result, "data");
+//                    outputList(list);
+//                    break;
+                case BROWSE:
+                    re.setRequestId("title");
                     list = re.json(result, "data");
                     outputList(list);
                     break;
@@ -125,7 +135,25 @@ public class ScheduleFunction {
         }
     }
 
+    private void outputMessage(String message){
+        MainActivity.speakText.setText(message);
+        MainActivity.tts.speak(message, TextToSpeech.QUEUE_ADD, null, message);
+    }
 
+
+    public void outputList(List<Map<String,String>> listDate){
+//        String[] from = {"title"};
+//        int[] to = {android.R.id.text1};
+//        SimpleAdapter adapter = new SimpleAdapter(main, listDate, android.R.layout.simple_list_item_1, from, to);
+//        _list.setAdapter(adapter);
+//        _list.setOnItemClickListener(new ListViewOnClickListener());
+//        MainActivity.tts.speak(Voice.voiceTodo, TextToSpeech.QUEUE_ADD, null, Voice.voiceTodo);
+        MainActivity.tts.speak(DATENAME+Voice.voiceSchedule, TextToSpeech.QUEUE_ADD, null, DATENAME+Voice.voiceSchedule);
+        for(Map<String, String> map: listDate) {
+            String message = map.get("title");
+            MainActivity.tts.speak(message, TextToSpeech.QUEUE_ADD, null, message);
+        }
+    }
 
 
 
