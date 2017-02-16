7161f0e4268e82b8a7efe5064a258b3141704e01
ueshima <re-to-mikan@hotmail.co.jp>
2017-02-16 15:38:49 +0900

    add スケジュール登録用sql追加

---
+++ MyKindredsServerSide/WebContent/WEB-INF/sql/ScheduleInsert.sql
@@ -0,0 +1,20 @@
+INSERT INTO schedule(
+  id,
+  date,
+  line,
+  content
+ )
+ VALUES(
+  /*if(USER_ID)start*/
+  USER_ID,
+  /*if(USER_ID)end*/
+  /*if(DATE)start*/
+  DATE,
+  /*if(DATE)end*/
+  /*if(LINE)start*/
+  LINE,
+  /*if(LINE)end*/
+  /*if(CONTENT)start*/
+  CONTENT
+  /*if(CONTENT)end*/
+ );
